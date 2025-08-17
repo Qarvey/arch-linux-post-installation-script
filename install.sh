dfebe2a0ad3b0ce74a72e4a98f616e56e912c301
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-08-18 05:05:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $HOME
 
 mkdir -p $HOME/.mnt/samsung
-sudo echo "LABEL=storage  /home/quijada/.mnt/samsung  btrfs  noatime,compress=zstd  0 0" >> /etc/fstab
+sudo echo "LABEL=storage  /home/quijada/.mnt/samsung  btrfs  noatime,compress=zstd,discard=async  0 0" >> /etc/fstab
 
 mkdir -p $HOME/.mnt/btrfs_hdd
-sudo echo "LABEL=btrfs_hdd  /home/quijada/.mnt/btrfs_hdd  btrfs  noatime,compress=zstd  0 0" >> /etc/fstab
+sudo echo "LABEL=btrfs_hdd  /home/quijada/.mnt/btrfs_hdd  btrfs  noatime,compress=zstd,discard=async  0 0" >> /etc/fstab
 
 ln -s $HOME/.mnt/btrfs_hdd/@files/Documents $HOME/Documents
 ln -s $HOME/.mnt/btrfs_hdd/@files/Downloads $HOME/Downloads
@@ -28,12 +28,20 @@ ${SCRIPT_DIR}/display-managers/lidm/lidm-service.sh
 
 yay -S metapac
 mkdir -p $HOME/.config/metapac/groups
-cp ${SCRIPT_DIR}/config.toml $HOME/.config/metapac/config.toml
+cp -v ${SCRIPT_DIR}/config.toml $HOME/.config/metapac/config.toml
 cp -rv ${SCRIPT_DIR}/groups/ $HOME/.config/metapac/groups/
 metapac unmanaged > $HOME/.config/metapac/groups/all.toml
 metapac sync
 
 echo 'EDITOR=micro' >> $HOME/.bashrc
+read -r -d '' FISH_SNIPPET <<'EOF'
+if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} && ${SHLVL} == 1 ]]
+then
+    shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
+    exec fish $LOGIN_OPTION
+fi
+EOF
+echo -e '$FISH_SNIPPET' >> $HOME/.bashrc
 source $HOME/.bashrc
 
 USER=$(whoami)
@@ -41,6 +49,12 @@ if ! getent group realtime > /dev/null; then
     sudo groupadd realtime
 fi
 sudo usermod -aG realtime "$USER"
+
+mkdir -p $HOME/.config/mpv
+echo 'hwdec=auto' >> $HOME/.config/mpv/mpv.conf
+
+mkdir -p $HOME/.config/hypr
+cp -rv ${SCRIPT_DIR}/hypr/ $HOME/.config/hypr/
 
 # sudo systemctl stop wpa_supplicant
 # sudo systemctl disable wpa_supplicant
