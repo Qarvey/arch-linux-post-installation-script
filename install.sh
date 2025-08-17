2efba8f771388d8232d01495778c42b4f5a37ef6
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-08-17 21:20:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd $HOME
+
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -11,3 +12,9 @@ makepkg -si
 ${SCRIPT_DIR}/display-managers/lidm/lidm-service.sh
 
 yay -S metapac
+mkdir -p $HOME/.config/metapac/groups
+cp ${SCRIPT_DIR}/config.toml $HOME/.config/metapac/config.toml
+cp -rv ${SCRIPT_DIR}/groups/ $HOME/.config/metapac/groups/
+metapac unmanaged > $HOME/.config/metapac/groups/all.toml
+metapac sync
+
