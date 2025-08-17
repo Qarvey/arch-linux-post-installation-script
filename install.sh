20af4dca5484f24f2c2ca2080501bba7fbaf7630
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-08-17 21:09:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd $HOME
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+${SCRIPT_DIR}/display-managers/lidm/lidm-service.sh
+
+yay -S metapac
