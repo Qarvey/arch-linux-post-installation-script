82e5cdb26db57355d9d4cb5aeeb4f01ceea3b9d0
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-08-17 21:11:52 +0800

    Update lidm-service.sh

--- display-managers/lidm/lidm-service.sh
+++ display-managers/lidm/lidm-service.sh
@@ -2,5 +2,4 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-sudo mkdir -p /etc/systemd/system/lidm.service
 sudo cp ${SCRIPT_DIR}/systemd.service /etc/systemd/system/lidm.service
