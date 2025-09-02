96e0277a5186004db665be0aca80c4550aa8873a
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-02 20:49:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ yay -S metapac
 mkdir -p $HOME/.config/metapac/groups
 cp -v ${SCRIPT_DIR}/config.toml $HOME/.config/metapac/config.toml
 cp -rv ${SCRIPT_DIR}/groups/ $HOME/.config/metapac/groups/
-metapac unmanaged > $HOME/.config/metapac/groups/all.toml
+metapac unmanaged > $HOME/.config/metapac/groups/minimal-cachyos.toml
 metapac sync
 
 xdg-user-dirs-update
