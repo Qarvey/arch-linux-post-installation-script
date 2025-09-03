1c7ad343dc9b4e58c2e3a0e373bc0d222364ab58
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-03 19:33:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,13 @@ ROOT_DEVICE=$(findmnt -n -o SOURCE / | sed 's/\[.*\]//')
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PART1_FLAG="${SCRIPT_DIR}/flags/part1.flag"
 
-[[ -e ${PART1_FLAG} ]] && echo "Continuing setup..."
+if [[ -e ${PART1_FLAG} ]]; then
+    echo "Continuing setup..."
+else
+    chmod +x ${SCRIPT_DIR}/scripts/*.sh
+fi
 
 sudo -v
-
-chmod +x ${SCRIPT_DIR}/scripts/*.sh
 
 # PART 1
 
@@ -18,68 +20,63 @@ if [[ ! -e ${PART1_FLAG} ]]; then
     source ${SCRIPT_DIR}/scripts/setup-storage.sh
     source ${SCRIPT_DIR}/scripts/setup-btrfs-swap.sh
     source ${SCRIPT_DIR}/scripts/setup-metapac.sh
-    touch ${PART1_FLAG}
-
-    echo "${SCRIPT_DIR}/setup.sh" | sudo tee -a ~/.bash_profile
     
+    touch ${PART1_FLAG}
+    echo "${SCRIPT_DIR}/setup.sh" | sudo tee -a $HOME/.bash_profile
     reboot
 fi
 
 # PART 2
 
-echo "Adding groups 'desktop' and 'hyprland' to `metapac` declaration..."
-METAPAC_CONFIG="${SCRIPT_DIR}/config.toml"
-NEW_METAPAC_GROUPS=("desktop" "hyprland")
-for val in "${NEW_METAPAC_GROUPS[@]}"; do
-    sed -i "/^\[hostname_groups\]/,/^\[/{ 
-        /^$(hostname) = \[/,/]/{
-            /]/i \  \"${val}\",
-        }
-    }" "${METAPAC_CONFIG}"
-done
-cp -v ${METAPAC_CONFIG} $HOME/.config/metapac/config.toml
+PART2_FLAG="${SCRIPT_DIR}/flags/part2.flag"
 
-echo "Attempting to install packages declared in the new `metapac` groups..."
-metapac sync
+if [[ ! -e ${PART2_FLAG} ]]; then
+    source ${SCRIPT_DIR}/scripts/declare-hyprland-desktop.sh
+    source ${SCRIPT_DIR}/scripts/setup-user-dirs.sh
+    
+    echo "Attempting to create group 'realtime'..."
+    (getent group realtime > /dev/null) || sudo groupadd realtime
+    echo "Adding user '$(whoami)' to group 'realtime'..."
+    sudo usermod -aG realtime "$(whoami)"
+    
+    echo "Enabling `mpv` hardware acceleration..."
+    mkdir -p $HOME/.config/mpv
+    echo 'hwdec=auto' >> $HOME/.config/mpv/mpv.conf
+    
+    echo "Configuring Hyprland..."
+    mkdir -p $HOME/.config/hypr
+    cp -rv ${SCRIPT_DIR}/hypr/ $HOME/.config/hypr/
+    
+    echo "Disabling `wpa_supplicant`..."
+    sudo systemctl stop wpa_supplicant
+    sudo systemctl disable wpa_supplicant
+    sudo systemctl mask wpa_supplicant
+    
+    echo "Replacing `wpa_supplicant` with `iwd` as WiFi backend..."
+    echo -e "[device]\nwifi.backend=iwd" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+    sudo systemctl restart NetworkManager
+    
+    echo "Uninstalling `wpa_supplicant`..."
+    yay -Rns --noconfirm wpa_supplicant
+    
+    echo "Loading the `i2c-dev` module..."
+    sudo modprobe i2c-dev
+    echo "Adding user '$(whoami)' to group 'i2c'..."
+    sudo usermod -aG i2c $(whoami)
+    
+    echo "Setting `micro` as default text editor for Fish..."
+    echo 'set -gx EDITOR micro' >> $HOME/.config/fish/config.fish
+    # echo 'set -gx VISUAL micro' >> $HOME/.config/fish/config.fish
+    
+    sed -i "\|^${SCRIPT_DIR}/setup.sh\$|d" "$HOME/.bash_profile"
+    touch ${PART2_FLAG}
 
-echo "Symlinking user directories in '~/.mnt/$WD_1TB_LABEL/' to home directory..."
-rm -rf $HOME/Documents
-ln -s $WD_1TB_MOUNTPOINT/@files/Documents $HOME/Documents
-
-rm -rf $HOME/Downloads
-ln -s $WD_1TB_MOUNTPOINT/@files/Downloads $HOME/Downloads
-
-rm -rf $HOME/Pictures
-ln -s $WD_1TB_MOUNTPOINT/@files/Pictures $HOME/Pictures
-
-rm -rf $HOME/Videos
-ln -s $WD_1TB_MOUNTPOINT/@files/Videos $HOME/Videos
-
-xdg-user-dirs-update
-
-USER=$(whoami)
-if ! getent group realtime > /dev/null; then
-    sudo groupadd realtime
+    echo "Setup complete!"
+    for ((i=10; i>0; i--)); do
+        echo -ne "\rRebooting in $i seconds... "
+        sleep 1
+    done
+    reboot
+else
+    echo "Setup already complete."
 fi
-sudo usermod -aG realtime "$USER"
-
-mkdir -p $HOME/.config/mpv
-echo 'hwdec=auto' >> $HOME/.config/mpv/mpv.conf
-
-mkdir -p $HOME/.config/hypr
-cp -rv ${SCRIPT_DIR}/hypr/ $HOME/.config/hypr/
-
-sudo systemctl stop wpa_supplicant
-sudo systemctl disable wpa_supplicant
-sudo systemctl mask wpa_supplicant
-
-echo -e "[device]\nwifi.backend=iwd" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
-sudo systemctl restart NetworkManager
-
-yay -Rns --noconfirm wpa_supplicant
-
-sudo modprobe i2c-dev
-sudo usermod -aG i2c $USER
-
-echo 'set -gx EDITOR micro' >> $HOME/.config/fish/config.fish
-# echo 'set -gx VISUAL micro' >> $HOME/.config/fish/config.fish
