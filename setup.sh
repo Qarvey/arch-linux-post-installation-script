a77c8700605731af112da9ae488c1243e5475478
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-03 14:06:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,36 +1,83 @@
 #!/bin/bash
 
+set -euo pipefail
+
+sudo -v
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $HOME
 
-mkdir -p $HOME/.mnt/SAMSUNG
-mkdir -p $HOME/.mnt/WDC
+mkdir -p $HOME/.mnt/SAMSUNG@STORAGE
+mkdir -p $HOME/.mnt/WD-1TB
 
-echo -e "\nUUID=$(lsblk -no UUID "$(findmnt -n -o SOURCE / | sed 's/\[.*\]//')")  /home/quijada/.mnt/SAMSUNG  btrfs  subvol=/@storage,defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
-echo "LABEL=WDC  /home/quijada/.mnt/WDC  btrfs  defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
+echo -e "\nUUID=$(lsblk -no UUID "$(findmnt -n -o SOURCE / | sed 's/\[.*\]//')")  /home/quijada/.mnt/SAMSUNG@STORAGE  btrfs  subvol=/@storage,defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
+echo "LABEL=WD-1TB  /home/quijada/.mnt/WD-1TB  btrfs  defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
 
 rm -rf $HOME/Documents
 rm -rf $HOME/Downloads
 rm -rf $HOME/Pictures
 rm -rf $HOME/Videos
 
-ln -s $HOME/.mnt/WDC/@files/Documents $HOME/Documents
-ln -s $HOME/.mnt/WDC/@files/Downloads $HOME/Downloads
-ln -s $HOME/.mnt/WDC/@files/Pictures $HOME/Pictures
-ln -s $HOME/.mnt/WDC/@files/Videos $HOME/Videos
+ln -s $HOME/.mnt/WD-1TB/@files/Documents $HOME/Documents
+ln -s $HOME/.mnt/WD-1TB/@files/Downloads $HOME/Downloads
+ln -s $HOME/.mnt/WD-1TB/@files/Pictures $HOME/Pictures
+ln -s $HOME/.mnt/WD-1TB/@files/Videos $HOME/Videos
 
-sudo pacman -S --noconfirm --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
+echo "Updating system..."
+sudo pacman -Syu --no-confirm
+echo "System updated."
 
-yay -S --noconfirm metapac
+if pacman -Q yay &>/dev/null; then
+    echo "`paru` found. Attempting to uninstall..."
+    sudo -Rns --no-confirm paru
+    echo "`paru` uninstalled."
+fi
+
+echo "Attempting to install `yay`..."
+
+if ! pacman -Q yay &>/dev/null; then
+    sudo pacman -S --noconfirm --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ..
+    rm -rf yay
+    echo "`yay` installed."
+else
+    echo "`yay` is already installed."
+fi
+
+echo "Attempting to install `metapac`..."
+
+if ! pacman -Q metapac &>/dev/null; then
+    yay -S --noconfirm metapac
+    echo "`metapac` installed."
+else
+    echo "`metapac` is already installed."
+fi
+
+echo "Initializing `metapac` configuration..."
+
+rm -rf $HOME/.config/metapac
 mkdir -p $HOME/.config/metapac/groups
 cp -v ${SCRIPT_DIR}/config.toml $HOME/.config/metapac/config.toml
 cp -rv ${SCRIPT_DIR}/groups/. $HOME/.config/metapac/groups/
-metapac unmanaged > $HOME/.config/metapac/groups/minimal-cachyos-base.toml
+
+if [[ -e ${SCRIPT_DIR}/groups/minimal-cachyos-base.toml ]]; then
+    while true; do
+        echo "File 'minimal-cachyos-base.toml' already exists. It contains all the packages in your system."
+        read -t 10 -rp "Regenerate? (y/N) [default No in 10s]: " ANSWER
+        case "$ANSWER" in
+            [Yy]* ) metapac unmanaged > ${SCRIPT_DIR}/groups/minimal-cachyos-base.toml; break;;
+            [Nn]* | "" ) break;;
+            * ) echo "Please answer with [y]es or [n]o."
+        esac
+    done
+fi
+cp -v ${SCRIPT_DIR}/groups/minimal-cachyos-base.toml $HOME/.config/metapac/groups/minimal-cachyos-base.toml
+
+echo "`metapac` configured. Attempting to install packages declared by `metapac`..."
+
 metapac sync
 
 xdg-user-dirs-update
