24b52115b8d59d83d10f79c4ba783d0ce6019d6b
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-03 22:59:13 +0800

    Delete scripts/declare-hyprland-desktop.sh

--- scripts/declare-hyprland-desktop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Adding groups 'desktop' and 'hyprland' to 'metapac' declaration..."
-METAPAC_CONFIG="${SCRIPT_DIR}/part2-config.toml"
-sed -i "s/^PLACEHOLDER = \[/$(hostname) = [/" "${METAPAC_CONFIG}"
-cp -v ${METAPAC_CONFIG} $HOME/.config/metapac/config.toml
-
-echo "Attempting to install packages declared in the new 'metapac' groups..."
-metapac sync
