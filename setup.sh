9d97eb4e6adf07fa0ba68a6ff0fbf5c863825588
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-03 15:12:34 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
 
 set -euo pipefail
-
 sudo -v
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 cd $HOME
 
-mkdir -p $HOME/.mnt/SAMSUNG@STORAGE
-mkdir -p $HOME/.mnt/WD-1TB
+SAMSUNG_STORAGE_UUID = "UUID=$(lsblk -no UUID "$(findmnt -n -o SOURCE / | sed 's/\[.*\]//')")"
+SAMSUNG_STORAGE_MOUNTPOINT = "$HOME/.mnt/SAMSUNG@STORAGE"
+mkdir -p $SAMSUNG_STORAGE_MOUNTPOINT
+if grep -q "UUID=${SAMSUNG_STORAGE_UUID}.*${SAMSUNG_STORAGE_MOUNTPOINT}" /etc/fstab; then
+    echo "UUID ${SAMSUNG_STORAGE_UUID} is already configured to mount at '${SAMSUNG_STORAGE_MOUNTPOINT}' in `fstab`."
+else
+    echo "Configuring UUID ${SAMSUNG_STORAGE_UUID} to mount at '${SAMSUNG_STORAGE_MOUNTPOINT}' in `fstab`..."
+    echo -e "\nUUID=${SAMSUNG_STORAGE_UUID}  ${SAMSUNG_STORAGE_MOUNTPOINT}  btrfs  subvol=/@storage,defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
+fi
 
-echo -e "\nUUID=$(lsblk -no UUID "$(findmnt -n -o SOURCE / | sed 's/\[.*\]//')")  /home/quijada/.mnt/SAMSUNG@STORAGE  btrfs  subvol=/@storage,defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
-echo "LABEL=WD-1TB  /home/quijada/.mnt/WD-1TB  btrfs  defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
+WD_1TB_LABEL = "WD-1TB"
+WD_1TB_MOUNTPOINT = "$HOME/.mnt/WD-1TB"
+mkdir -p $WD_1TB_MOUNTPOINT
+if grep -q "LABEL=${WD_1TB_LABEL}.*${WD_1TB_MOUNTPOINT}" /etc/fstab; then
+    echo "LABEL ${WD_1TB_LABEL} is already configured to mount at '${WD_1TB_MOUNTPOINT}' in `fstab`."
+else
+    echo "Configuring LABEL ${WD_1TB_LABEL} to mount at '${WD_1TB_MOUNTPOINT}' in `fstab`..."
+    echo "LABEL=${WD_1TB_LABEL}  ${WD_1TB_MOUNTPOINT}  btrfs  defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
+fi
 
+echo "Symlinking user directories in '~/.mnt/$WD_1TB_LABEL/' to home directory..."
 rm -rf $HOME/Documents
-rm -rf $HOME/Downloads
-rm -rf $HOME/Pictures
-rm -rf $HOME/Videos
+ln -s $WD_1TB_MOUNTPOINT/@files/Documents $HOME/Documents
 
-ln -s $HOME/.mnt/WD-1TB/@files/Documents $HOME/Documents
-ln -s $HOME/.mnt/WD-1TB/@files/Downloads $HOME/Downloads
-ln -s $HOME/.mnt/WD-1TB/@files/Pictures $HOME/Pictures
-ln -s $HOME/.mnt/WD-1TB/@files/Videos $HOME/Videos
+rm -rf $HOME/Downloads
+ln -s $WD_1TB_MOUNTPOINT/@files/Downloads $HOME/Downloads
+
+rm -rf $HOME/Pictures
+ln -s $WD_1TB_MOUNTPOINT/@files/Pictures $HOME/Pictures
+
+rm -rf $HOME/Videos
+ln -s $WD_1TB_MOUNTPOINT/@files/Videos $HOME/Videos
 
 echo "Updating system..."
 sudo pacman -Syu --no-confirm
-echo "System updated."
 
-if pacman -Q yay &>/dev/null; then
+if pacman -Q paru &>/dev/null; then
     echo "`paru` found. Attempting to uninstall..."
     sudo -Rns --no-confirm paru
-    echo "`paru` uninstalled."
 fi
 
 echo "Attempting to install `yay`..."
-
 if ! pacman -Q yay &>/dev/null; then
     sudo pacman -S --noconfirm --needed git base-devel
     git clone https://aur.archlinux.org/yay.git
@@ -42,27 +55,22 @@ if ! pacman -Q yay &>/dev/null; then
     makepkg -si
     cd ..
     rm -rf yay
-    echo "`yay` installed."
 else
     echo "`yay` is already installed."
 fi
 
 echo "Attempting to install `metapac`..."
-
 if ! pacman -Q metapac &>/dev/null; then
     yay -S --noconfirm metapac
-    echo "`metapac` installed."
 else
     echo "`metapac` is already installed."
 fi
 
 echo "Initializing `metapac` configuration..."
-
 rm -rf $HOME/.config/metapac
 mkdir -p $HOME/.config/metapac/groups
 cp -v ${SCRIPT_DIR}/config.toml $HOME/.config/metapac/config.toml
 cp -rv ${SCRIPT_DIR}/groups/. $HOME/.config/metapac/groups/
-
 if [[ -e ${SCRIPT_DIR}/groups/minimal-cachyos-base.toml ]]; then
     while true; do
         echo "File 'minimal-cachyos-base.toml' already exists. It contains all the packages in your system."
@@ -76,8 +84,7 @@ if [[ -e ${SCRIPT_DIR}/groups/minimal-cachyos-base.toml ]]; then
 fi
 cp -v ${SCRIPT_DIR}/groups/minimal-cachyos-base.toml $HOME/.config/metapac/groups/minimal-cachyos-base.toml
 
-echo "`metapac` configured. Attempting to install packages declared by `metapac`..."
-
+echo "Attempting to install packages declared in the `metapac` groups..."
 metapac sync
 
 xdg-user-dirs-update
