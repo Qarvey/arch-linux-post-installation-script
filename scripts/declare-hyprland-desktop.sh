91e13f20eb7a93293009f95f3fffd4beb1607d9a
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-03 21:59:40 +0800

    Update declare-hyprland-desktop.sh

--- scripts/declare-hyprland-desktop.sh
+++ scripts/declare-hyprland-desktop.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 echo "Adding groups 'desktop' and 'hyprland' to 'metapac' declaration..."
-METAPAC_CONFIG="${SCRIPT_DIR}/config.toml"
-NEW_METAPAC_GROUPS=("desktop" "hyprland")
-for val in "${NEW_METAPAC_GROUPS[@]}"; do
-    sed -i "/^\[hostname_groups\]/,/^\[/{ 
-        /^$(hostname) = \[/,/]/{
-            /]/i \  \"${val}\",
-        }
-    }" "${METAPAC_CONFIG}"
-done
+METAPAC_CONFIG="${SCRIPT_DIR}/part2-config.toml"
+sed -i "s/^PLACEHOLDER = \[/$(hostname) = [/" "${METAPAC_CONFIG}"
 cp -v ${METAPAC_CONFIG} $HOME/.config/metapac/config.toml
 
 echo "Attempting to install packages declared in the new 'metapac' groups..."
