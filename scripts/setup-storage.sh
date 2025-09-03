27f7c228930bd7cb310add2c11195b7d3abc0be6
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-03 20:16:42 +0800

    Update setup-storage.sh

--- scripts/setup-storage.sh
+++ scripts/setup-storage.sh
@@ -16,9 +16,9 @@ SAMSUNG_STORAGE_MOUNTPOINT="$HOME/.mnt/SAMSUNG@STORAGE"
 echo "Creating mountpoint ${SAMSUNG_STORAGE_MOUNTPOINT}..."
 mkdir -p ${SAMSUNG_STORAGE_MOUNTPOINT}
 if grep -q "UUID=${SAMSUNG_STORAGE_UUID}.*${SAMSUNG_STORAGE_MOUNTPOINT}" /etc/fstab; then
-    echo "UUID ${SAMSUNG_STORAGE_UUID} is already configured to mount at '${SAMSUNG_STORAGE_MOUNTPOINT}' in `fstab`."
+    echo "UUID ${SAMSUNG_STORAGE_UUID} is already configured to mount at '${SAMSUNG_STORAGE_MOUNTPOINT}' in 'fstab'."
 else
-    echo "Configuring UUID ${SAMSUNG_STORAGE_UUID} to mount at '${SAMSUNG_STORAGE_MOUNTPOINT}' in `fstab`..."
+    echo "Configuring UUID ${SAMSUNG_STORAGE_UUID} to mount at '${SAMSUNG_STORAGE_MOUNTPOINT}' in 'fstab'..."
     echo -e "\nUUID=${SAMSUNG_STORAGE_UUID}  ${SAMSUNG_STORAGE_MOUNTPOINT}  btrfs  subvol=/@storage,defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
 fi
 
@@ -27,8 +27,8 @@ WD_1TB_MOUNTPOINT="$HOME/.mnt/WD-1TB"
 echo "Creating mountpoint ${WD_1TB_MOUNTPOINT}..."
 mkdir -p ${WD_1TB_MOUNTPOINT}
 if grep -q "LABEL=${WD_1TB_LABEL}.*${WD_1TB_MOUNTPOINT}" /etc/fstab; then
-    echo "LABEL ${WD_1TB_LABEL} is already configured to mount at '${WD_1TB_MOUNTPOINT}' in `fstab`."
+    echo "LABEL ${WD_1TB_LABEL} is already configured to mount at '${WD_1TB_MOUNTPOINT}' in 'fstab'."
 else
-    echo "Configuring LABEL ${WD_1TB_LABEL} to mount at '${WD_1TB_MOUNTPOINT}' in `fstab`..."
+    echo "Configuring LABEL ${WD_1TB_LABEL} to mount at '${WD_1TB_MOUNTPOINT}' in 'fstab'..."
     echo "LABEL=${WD_1TB_LABEL}  ${WD_1TB_MOUNTPOINT}  btrfs  defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
 fi
