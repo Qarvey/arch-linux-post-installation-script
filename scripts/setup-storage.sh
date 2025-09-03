c59b33b0ddb975370f7116f6f2029141d002ed20
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-09-03 19:54:20 +0800

    Update setup-storage.sh

--- scripts/setup-storage.sh
+++ scripts/setup-storage.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "Creating Btrfs subvolume '/@storage' in '${ROOT_DEVICE}'..."
-sudo btrfs subvolume create /@storage
+TEMP_MOUNTPOINT=$(mktemp -d /tmp/mnt.XXXXXX)
+sudo mount -o subvolid=5 ${ROOT_DEVICE} ${TEMP_MOUNTPOINT}
+if [ -d "${TEMP_MOUNTPOINT}/@storage" ]; then
+    echo "Btrfs subvolume '/@storage' already exists in '${ROOT_DEVICE}'..."
+else
+    echo "Creating Btrfs subvolume '/@storage' in '${ROOT_DEVICE}'..."
+    sudo btrfs subvolume create ${TEMP_MOUNTPOINT}/@storage
+fi
+sudo umount ${TEMP_MOUNTPOINT}
+rmdir ${TEMP_MOUNTPOINT}
 
-SAMSUNG_STORAGE_UUID = $(lsblk -no UUID ${ROOT_DEVICE})
-SAMSUNG_STORAGE_MOUNTPOINT = "$HOME/.mnt/SAMSUNG@STORAGE"
+SAMSUNG_STORAGE_UUID=$(lsblk -no UUID ${ROOT_DEVICE})
+SAMSUNG_STORAGE_MOUNTPOINT="$HOME/.mnt/SAMSUNG@STORAGE"
 echo "Creating mountpoint ${SAMSUNG_STORAGE_MOUNTPOINT}..."
 mkdir -p ${SAMSUNG_STORAGE_MOUNTPOINT}
 if grep -q "UUID=${SAMSUNG_STORAGE_UUID}.*${SAMSUNG_STORAGE_MOUNTPOINT}" /etc/fstab; then
@@ -14,8 +22,8 @@ else
     echo -e "\nUUID=${SAMSUNG_STORAGE_UUID}  ${SAMSUNG_STORAGE_MOUNTPOINT}  btrfs  subvol=/@storage,defaults,noatime,compress=zstd,commit=120  0 0" | sudo tee -a /etc/fstab
 fi
 
-WD_1TB_LABEL = "WD-1TB"
-WD_1TB_MOUNTPOINT = "$HOME/.mnt/WD-1TB"
+WD_1TB_LABEL="WD-1TB"
+WD_1TB_MOUNTPOINT="$HOME/.mnt/WD-1TB"
 echo "Creating mountpoint ${WD_1TB_MOUNTPOINT}..."
 mkdir -p ${WD_1TB_MOUNTPOINT}
 if grep -q "LABEL=${WD_1TB_LABEL}.*${WD_1TB_MOUNTPOINT}" /etc/fstab; then
