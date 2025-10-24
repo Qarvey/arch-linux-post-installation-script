83d5b3f7862e39eceddc473b628e58aa291d8414
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-10-24 15:24:22 +0800

    Update setup-btrfs-swap.sh

--- scripts/setup-btrfs-swap.sh
+++ scripts/setup-btrfs-swap.sh
@@ -28,4 +28,4 @@ echo "Activating '/swap/swapfile'..."
 sudo swapon /swap/swapfile
 echo "/swap/swapfile  none  swap  defaults,discard  0 0" | sudo tee -a /etc/fstab
 
-touch "${FLAGS}/${SCRIPT}.flag"
+touch ${SCRIPT_FLAG}
