51b68b7acfe5b400a27dd6e0884163e7ce76fea3
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-11-22 14:15:49 +0800

    Update setup-misc.sh

--- scripts/setup-misc.sh
+++ scripts/setup-misc.sh
@@ -13,4 +13,10 @@ echo 'export EDITOR="micro"' >> $HOME/.bashrc
 
 echo 'fastfetch' >> $HOME/.bashrc
 
+mkdir -p $HOME/.local/share/PrismLauncher
+ln -s $HOME/.mnt/SAMSUNG@STORAGE/minecraft-instances $HOME/.local/share/PrismLauncher/instances
+
+cp -rv ${SCRIPT_DIR}/bin $HOME/
+cp -rv ${SCRIPT_DIR}/desktop $HOME/.local/share/applications
+
 touch ${SCRIPT_FLAG}
