5d47f6ac16d9edc3fbb1b95626311cdd12591ac8
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-11-24 00:37:22 +0800

    Update setup-misc.sh

--- scripts/setup-misc.sh
+++ scripts/setup-misc.sh
@@ -15,6 +15,7 @@ echo 'export VISUAL="code"' >> $HOME/.bashrc
 echo 'fastfetch' >> $HOME/.bashrc
 
 mkdir -p $HOME/.local/share/PrismLauncher
+rm -rf $HOME/.local/share/PrismLauncher/instances
 ln -s $HOME/.mnt/SAMSUNG@STORAGE/minecraft-instances $HOME/.local/share/PrismLauncher/instances
 
 cp -rv ${SCRIPT_DIR}/bin $HOME/
@@ -24,6 +25,7 @@ rm -rf $HOME/Desktop
 ln -s $HOME/.local/share/applications $HOME/Desktop
 
 sudo mkdir -p /etc/xdg/reflector
+cp -rv /etc/xdg/reflector/reflector.conf /etc/xdg/reflector/reflector.conf.bak
 cp -rv ${SCRIPT_DIR}/etc/xdg/reflector/reflector.conf /etc/xdg/reflector/reflector.conf
 
 touch ${SCRIPT_FLAG}
