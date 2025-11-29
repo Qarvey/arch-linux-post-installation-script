a3aae22415d76b3cea647aadc66fd24444be424e
Qarvey <108917586+Qarvey@users.noreply.github.com>
2025-11-29 09:11:13 +0800

    Update setup-snapper.sh

--- scripts/setup-snapper.sh
+++ scripts/setup-snapper.sh
@@ -13,31 +13,23 @@ else
   sudo btrfs subvolume create ${TEMP_MOUNTPOINT}/@root_snapshots
 fi
 
-# if [ -d "${TEMP_MOUNTPOINT}/@home_snapshots" ]; then
-#   echo "Btrfs subvolume '/@home_snapshots' already exists in '${ROOT_DEVICE}'."
-# else
-#   echo "Creating Btrfs subvolume '/@home_snapshots' in '${ROOT_DEVICE}'..."
-#   sudo btrfs subvolume create ${TEMP_MOUNTPOINT}/@home_snapshots
-# fi
-
 sudo umount ${TEMP_MOUNTPOINT}
 rmdir ${TEMP_MOUNTPOINT}
-
-if mountpoint -q /.snapshots; then
-    echo "Unmounting existing mount at '/.snapshots'..."
-    sudo umount /.snapshots
-fi
-
-sudo rm -rf /.snapshots
 
 echo "Creating Snapper configuration(s)..."
 if [ -f /etc/snapper/configs/root ]; then
     echo "Snapper config 'root' already exists. Skipping creation."
 else
+    if mountpoint -q /.snapshots; then
+        echo "Unmounting existing mount at '/.snapshots'..."
+        sudo umount /.snapshots
+    fi
+    
+    sudo rm -rf /.snapshots
+
     echo "Creating Snapper config 'root'..."
     sudo snapper -c root create-config /
 fi
-# sudo snapper -c home create-config /home
 
 if grep -q "UUID=${SAMSUNG_STORAGE_UUID}.*/.snapshots" /etc/fstab; then
     echo "SUBVOLUME '@root_snapshots' in UUID ${SAMSUNG_STORAGE_UUID} is already configured to mount at '/.snapshots' in 'fstab'."
